576bf7d233d62dd58aec5f00b126b466a89ca8a8
fumingxiaosheng <xiaowenhu_hust@163.com>
2024-03-01 10:00:08 +0800

    modify len of ctruprime

--- src/kem/ctruprime/kem_ctruprime.h
+++ src/kem/ctruprime/kem_ctruprime.h
@@ -7,7 +7,7 @@
 #define OQS_KEM_ctruprime_653_length_public_key 994
 #define OQS_KEM_ctruprime_653_length_secret_key 1353
 #define OQS_KEM_ctruprime_653_length_ciphertext 898
-#define OQS_KEM_ctruprime_653_length_shared_secret 33
+#define OQS_KEM_ctruprime_653_length_shared_secret 32
 OQS_KEM * OQS_KEM_ctruprime_653_new(void); //TODO1:
 OQS_API OQS_STATUS OQS_KEM_ctruprime_653_keypair(uint8_t *public_key,uint8_t *secret_key); //TODO2
 OQS_API OQS_STATUS OQS_KEM_ctruprime_653_encaps(uint8_t *ciphertext,uint8_t *shared_secret,const uint8_t *public_key);//TODO3
